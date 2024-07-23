6293057b7ff70a5531f6c64a18d71da771e4dbe7
Keshav Raj <keshavraj1022@gmail.com>
2024-07-23 18:45:37 +0530

    Write the HTML code to attach images of logos

--- Main/scripts.js
+++ Main/scripts.js
@@ -1,18 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const hoverItems = document.querySelectorAll(".hover-item");
-
-//     hoverItems.forEach(item => {
-//         item.addEventListener("mouseover", function() {
-//             this.style.color = "#a3cd39"; // Change text color on hover
-//         });
-//         item.addEventListener("mouseout", function() {
-//             this.style.color = "#333"; // Revert text color when not hovering
-//         });
-//     });
-// });
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const hoverItems = document.querySelectorAll(".hover-item");
     const contentSection = document.getElementById("content-section");
